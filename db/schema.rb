c665b8e4af4c7d450f43242c418dcb1a7f16260a
ArsemaYemiru <arsemayemiru610@gmail.com>
2023-12-28 15:09:26 +0300

    added genere and publisher model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_26_131529) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_28_120541) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
